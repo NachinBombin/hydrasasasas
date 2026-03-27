5d6111d7f4178d239c39801e4bb85a65d4f67df7
NachinBombin <solidnachox@gmail.com>
2026-03-27 19:15:45 -0300

    Reskin: Bombin Addons category, cable appearance, dark tint

--- lua/entities/hydra_stinger/shared.lua
+++ lua/entities/hydra_stinger/shared.lua
@@ -1,10 +1,10 @@
 
 ENT.Type 			= "anim"
 ENT.Base 			= "base_anim"
-ENT.PrintName		= "Hydra Stinger"
+ENT.PrintName		= "Cable Stinger"
 ENT.Author			= "Q42 (fixed by Jonascone)"
 ENT.Information		= "Kills NPCs and players that get too close."
-ENT.Category		= "Improved Hydra"
+ENT.Category		= "Bombin Addons"
 
-ENT.Spawnable			= false
-ENT.AdminSpawnable		= false
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= true
