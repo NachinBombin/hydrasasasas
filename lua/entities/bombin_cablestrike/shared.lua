f8e3176ac062b082825df427b7293ffb3746354c
NachinBombin <solidnachox@gmail.com>
2026-03-27 23:05:59 -0300

    Delete lua/entities/bombin_cablestrike directory

--- lua/entities/bombin_cablestrike/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Type            = "anim"
-ENT.Base            = "base_anim"
-ENT.PrintName       = "Cable Strike"
-ENT.Author          = "Q42 (fixed by Jonascone, reskinned by Bombin)"
-ENT.Information     = "Kills NPCs and players that get too close. Standalone cable variant."
-ENT.Category        = "Bombin Addons"
-
-ENT.Spawnable           = true
-ENT.AdminSpawnable      = true
